89652093cb16ef445d47ba6704ee748acadf7c52
lkononenko <liubov.kononenko@gmail.com>
2023-09-03 23:02:26 +0200

    feat: update agenda

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -46,6 +46,38 @@ export default function Home() {
           <AnimatedTitle>Upcoming Concerts</AnimatedTitle>
           <div className="max-w-screen-md mx-auto flex flex-col sm:flex-row items-center gap-4 justify-between my-10">
             <div className="text-center font-thin flex sm:flex-col items-end sm:items-center sm:basis-1/4">
+              <span className="text-4xl md:text-6xl text-primary mr-3 sm:mr-0">10</span>
+              <span className="text-2xl md:text-lg text-white mb-[2px]">September</span>
+            </div>
+            <div className="text-white text-center sm:text-left font-thin sm:basis-1/2">
+              <p className="text-lg font-light">Korenlint</p>
+              <div className="flex items-center gap-2 mb-2 sm:mb-4 justify-center sm:justify-normal">
+                <Image
+                  src={`/icons/location.svg`}
+                  alt="Instagram"
+                  width="25"
+                  height="25"
+                />
+                <span>Haarlem</span>
+              </div>
+              <div>
+                <span className="pr-2">14:00 - 14:20</span> <span>Waalse Kerk - Begijnhof 30</span>
+              </div>
+              <div>
+                <span className="pr-2">15:15 - 15:45</span> <span>RASOM - Gierstraat 5 (HEMA kelder)</span>
+              </div>
+            </div>
+            <div className="sm:basis-1/4">
+              <a
+                href="https://www.korenlint.nl/profiel/makivka"
+                className="btn btn-primary h-10 min-h-0"
+                target="_blank"
+              >Learn More</a>
+            </div>
+          </div>
+          <h2>Recent Concerts</h2>
+          <div className="max-w-screen-md mx-auto flex flex-col sm:flex-row items-center gap-4 justify-between my-10">
+            <div className="text-center font-thin flex sm:flex-col items-end sm:items-center sm:basis-1/4">
               <span className="text-4xl md:text-6xl text-primary mr-3 sm:mr-0">24</span>
               <span className="text-2xl md:text-lg text-white mb-[2px]">August</span>
             </div>
@@ -70,68 +102,6 @@ export default function Home() {
               >Learn More</a>
             </div>
           </div>
-
-          <div className="max-w-screen-md mx-auto flex flex-col sm:flex-row items-center gap-4 justify-between my-10">
-            <div className="text-center font-thin flex sm:flex-col items-end sm:items-center sm:basis-1/4">
-              <span className="text-4xl md:text-6xl text-primary mr-3 sm:mr-0">10</span>
-              <span className="text-2xl md:text-lg text-white mb-[2px]">September</span>
-            </div>
-            <div className="text-white text-center sm:text-left font-thin sm:basis-1/2">
-              <p className="text-lg font-light">Korenlint</p>
-              <div className="flex items-center gap-2 mb-2 sm:mb-4 justify-center sm:justify-normal">
-                <Image
-                  src={`/icons/location.svg`}
-                  alt="Instagram"
-                  width="25"
-                  height="25"
-                />
-                <span>Haarlem</span>
-              </div>
-              <div>
-                <span className="pr-2">14:00 - 14:20</span> <span>Waalse Kerk - Begijnhof 30</span>
-              </div>
-              <div>
-                <span className="pr-2">15:15 - 15:45</span> <span>HEMA - Grote Houtstraat 70</span>
-              </div>
-            </div>
-            <div className="sm:basis-1/4">
-              <a
-                href="https://www.korenlint.nl/profiel/makivka"
-                className="btn btn-primary h-10 min-h-0"
-                target="_blank"
-              >Learn More</a>
-            </div>
-          </div>
-          {/* <div className="card lg:card-side bg-base-100 shadow-xl max-w-screen-md mx-auto">
-            <figure>
-              <Image
-                src="/images/about-us.jpeg"
-                alt="Concert of Makivka"
-                width={300}
-                height={368}
-              />
-            </figure>
-            <div className="card-body">
-              <h3 className="card-title">Korenlint</h3>
-              <p>10 September 2023</p>
-              
-              <p>Haarlem</p>
-              
-              14:00 - 14:20 uur
-              Waalse Kerk
-              Begijnhof 30
-              15:15 - 15:45 uur
-              HEMA (kelder)
-              Grote Houtstraat 70
-              <div className="card-actions justify-end">
-                <a
-                  href="https://www.korenlint.nl/profiel/makivka"
-                  className="btn btn-primary"
-                  target="_blank"
-                >Learn More</a>
-              </div>
-            </div>
-          </div> */}
         </Section>
         <Section bgColor="bg-accent" id="team">
           <AnimatedTitle>Our Team</AnimatedTitle>
